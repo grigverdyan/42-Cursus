d741b64a0d23b331d8e7e9f00147d8b70dafea94
Grigori Verdyan <gverdyan@c2r15s6.42yerevan.am>
2022-08-23 19:44:51 +0400

    push swap

---
+++ push_swap/libft/ft_strncmp.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncmp.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gverdyan <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/17 04:08:10 by gverdyan          #+#    #+#             */
+/*   Updated: 2022/08/23 16:15:03 by dpetrosy         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_strncmp(const char *s1, const char *s2, size_t n)
+{
+	size_t	i;
+
+	if (!n)
+		return (0);
+	i = 0;
+	if ((s1[i] && !s2[i]) || (!s1[i] && s2[i]))
+		return ((unsigned char) s1[i] - (unsigned char) s2[i]);
+	while (i < n && (s1[i] || s2[i]))
+	{
+		if ((unsigned char)s1[i] != (unsigned char)s2[i])
+			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+		i++;
+	}
+	return (0);
+}
